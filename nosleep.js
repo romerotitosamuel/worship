53dd8a93af4548a1e65e8a6f73ee667f32f06e0e
tsamuel1289 <romerotitosamuel@gmail.com>
2019-07-18 14:38:28 -0400

    with no sleep

---
+++ nosleep.js
@@ -0,0 +1,9 @@
+var noSleep = new NoSleep();
+ 
+function enableNoSleep() {
+  noSleep.enable();
+  document.removeEventListener('touchstart', enableNoSleep, false);
+}
+ 
+// Habilitar el seguro para no bloquear la pantalla
+// document.addEventListener('touchstart', enableNoSleep, false)
