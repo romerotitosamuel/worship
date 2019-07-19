9ae8142d45b4a8148a63bf9bd089e8d0fc698d2f
unknown <romerotitosamuel@gmail.com>
2019-07-19 18:29:26 -0400

    traying JS getelement by id

--- nosleep.js
+++
@@ -1,9 +0,0 @@
-var noSleep = new NoSleep();
- 
-function enableNoSleep() {
-  noSleep.enable();
-  document.removeEventListener('touchstart', enableNoSleep, false);
-}
- 
-// Habilitar el seguro para no bloquear la pantalla
-document.addEventListener('touchstart', enableNoSleep, false)
