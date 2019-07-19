f4ff69c1c2d3fecadc54232422955c754a39c525
unknown <romerotitosamuel@gmail.com>
2019-07-19 19:05:11 -0400

    all right with JS

---
+++ data_song.js
@@ -0,0 +1,5 @@
+
+function tuguiasmidestino() {
+    document.getElementById("parrafo").innerHTML="<p>Tu guias mi destino<br>Tu guias mis paso<br>En ti solo confío<br>Hoy vengo a tus brazos</p><p>No tengo a nadie mas que a ti<br>Tu eres mi razon de vivir</p>";
+}
+
