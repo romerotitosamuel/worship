07f5470c0bb6c9d697604495bbbff5e56c138f85
unknown <romerotitosamuel@gmail.com>
2019-07-19 21:15:54 -0400

    Voces ready

--- data_song.js
+++ data_song.js
@@ -1,5 +1,8 @@
-
-function tuguiasmidestino() {
-    document.getElementById("parrafo").innerHTML="<p>Tu guias mi destino<br>Tu guias mis paso<br>En ti solo confío<br>Hoy vengo a tus brazos</p><p>No tengo a nadie mas que a ti<br>Tu eres mi razon de vivir</p>";
-}
-
+// T letter
+    function tuguiasmidestino() {
+       document.getElementById("song").innerHTML="<p><b>Tu guias mi destino</b><br>Josue del Cid - bpm:76</p><hr><p>Tu guías mi destino<br>      Tu guías mis pasos<br>En ti solo confío<br>Hoy vengo a tus brazos</p><p>No tengo a nadie mas que a ti<br>Tu eres mi razón de vivir</p><p>La fuente de mi vida eres tu<br>Mi amigo, mi consuelo eres tu<br>No tengo a nadie mas que a ti<br>Tu eres mi la razón de vivir<br>Solo tu</p>";
+   }
+// V letter
+    function vieneya() {
+        document.getElementById("song").innerHTML="<p><b>Viene ya</b><br>Miel San Marcos - bpm:150</p><hr><p>Preparemos el camino Cristo viene ya<br>Anunciemos su venida en todo lugar<br>Que se abran hoy las puertas el viene ya<br>Volveremos a adorarle por la eternidad</p><p>Viene ya mi amado, pronto le veré<br>Viene ya mi amado, pronto volverá</p><p>Voy cantando, gritando, celebrando su victoria<br>Viene ya mi amado, Cristo viene ya</p><p>Volveremos, cantando<br>Volveremos, saltando<br>Volveremos, con gozo<br>Volveremos gritando</p><p>Voy cantando, gritando, celebrando su victoria<br>Viene ya mi amado, Cristo viene ya<br>Cristo viene ya</p>";
+    }
