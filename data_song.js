c992253c53a657cee44644eb75edca44e2507217
tsamuel1289 <romerotitosamuel@gmail.com>
2019-07-23 15:30:13 -0400

    update until sunday 28 Jul for presentation

--- data_song.js
+++ data_song.js
@@ -1,3 +1,17 @@
+// A letter
+function adanzar() {
+    document.getElementById("song").innerHTML="<p><b>A danzar</b><br>Barak - bpm: 140</p><hr><p>A danzar delante de su presencia<br>Su amor es quien nos sustenta<br>Él es Dios<br>Él es quien resucitó<br>A danzar</p><hr><p>Da un paso a frente<br>Haz algo diferente<br>Demos toda gloria a Jesús</p><hr><p>Levanta tus manos<br>Grita que Él es santo<br>Es el gran yo soy<br>Murió en la cruz</p><hr><p>Si tu respiras tienes que adorar<br>Que nada te impida danzar</p>";
+}
+
+
+// D letter
+function diosincomparable() {
+    document.getElementById("song").innerHTML="<p><b>Dios incomparable</b><br>Generación 12 - bpm: 78</p><hr><p>Dios de mi corazón<br>en ti encontré mi salvación<br>tu gloria y majestad<br>quiero siempre contemplar</p><hr><p>Tu eres mi adoración<br>y mi eterna canción<br>todo mi interior<br>es cautivado por tu amor</p><hr><p>Eres Dios eterno<br>solo Tú eres bueno<br>Dios incomparable<br>eres Tú</p><hr><p>Nunca me separaré<br>de tu gran amor<br>eres mi Señor<br>mi Salvador</p>";
+}
+
+
+
+
 // G letter
 function glorioso() {
     document.getElementById("song").innerHTML="<p><b>Glorioso</b><br>BJ Putnam - bpm: 148</p><hr><p>¡Gl<u><small>A</small></u>ori<u><small>E</small></u>os<u><small>F#m-D</small></u>o! Te cantamos<br>¡Gl<u><small>A</small></u>ori<u><small>E</small></u>os<u><small>F#m-D</small></u>o! Lo gritamos<br><u><small>A</small></u>Cristo te al<u><small>E</small></u>abaré<br><u><small>F#m</small></u>Tu nombre ex<u><small>D</small></u>altaré<br>¡Gl<u><small>A</small></u>ori<u><small>E</small></u>os<u><small>D</small></u>o! Nadie como tú</p><p><sub>Intro: D-F#m-D-A-E</sub></p><hr><p><u><small>D</small></u>Tú r<u><small>A</small></u>einar<u><small>E</small></u>ás<br>Por si<u><small>F#m</small></u>empre y siempre<br><u><small>D</small></u>Tú v<u><small>A</small></u>ivir<u><small>E</small></u>ás</p><p><u><small>D</small></u>Tu am<u><small>A</small></u>or ser<u><small>E</small></u>á<br>Por si<u><small>F#m</small></u>empre y siempre<br><u><small>D</small></u>No c<u><small>A</small></u>ambiar<u><small>E</small></u>ás</p><hr><p>Gritar<u><small>D</small></u>é, gritaré<br>Porque bu<u><small>A</small></u>eno es Dios<br>Cantar<u><small>E</small></u>é, cantaré<br>Tú eres <u><small>F#m</small></u>El Señor<br><u><small>D</small></u>Declarar<u><small>A</small></u>é, nadie c<u><small>E</small></u>omo tú</p><hr><p><u><small>A</small></u>Tu luz brillar<u><small>C#7</small></u>á, todos ver<u><small>F#m</small></u>án<br>No hay nadie c<u><small>D</small></u>omo tú</p>";
@@ -6,10 +20,25 @@ function gloriaenloalto() {
     document.getElementById("song").innerHTML="<p><b>Gloria en lo alto</b><br>Christine dClario - bpm: 100</p><hr><p><sub>Intro: C-Em-F</sub></p><p>Quebrant<u><small>C</small></u>ado fuiste<br>oh Jes<u><small>Em</small></u>ús por mi rebeli<u><small>F</small></u>ón<br>Resucit<u><small>C</small></u>aste, y hoy puedo viv<u><small>E</small></u>ir<br>Y de nuevo nac<u><small>F</small></u>er</p><hr><p>Cu<u><small>Am</small></u>anto am<u><small>F</small></u>aste al mundo Di<u><small>C-G</small></u>os<br>Cu<u><small>Am</small></u>anto te qui<u><small>F</small></u>ero agradec<u><small>C-G</small></u>er</p><hr><p>Gl<u><small>Am</small></u>oria, gloria en lo <u><small>F</small></u>alto<br>Gloria en lo <u><small>C</small></u>alto, gloria al R<u><small>G</small></u>ey<br>Gl<u><small>Am</small></u>oria, gloria a Ti mi Cr<u><small>F</small></u>isto<br>Tu nombre por si<u><small>C</small></u>empre exaltar<u><small>G</small></u>é</p><hr><p>Venc<u><small>C</small></u>iste el mundo pecad<u><small>Em</small></u>or<br>Con tu gran am<u><small>F</small></u>or<br>Ahora el P<u><small>C</small></u>adre te ha exalt<u><small>Em</small></u>ado<br> al más alto lug<u><small>F</small></u>ar</p><hr><p>Cu<u><small>Am</small></u>anto am<u><small>F</small></u>aste al mundo Di<u><small>C-G</small></u>os<br>Cu<u><small>Am</small></u>anto te qui<u><small>F</small></u>ero enaltec<u><small>C-G</small></u>er</p><hr><p><sub>Puente: Dm-Am-Em-F-G</sub></p><p><u><small>Am</small></u>Digno, solo Tú eres d<u><small>F</small></u>igno<br>Solo Tú eres d<u><small>C</small></u>igno<br>Solo Tú eres Di<u><small>G</small></u>os</p>";
 }
 
+// L letter
+function libresoy() {
+    document.getElementById("song").innerHTML="<p><b>Libre soy</b><br>David Escarpeta - bpm: 133</p><hr><p>Por ti los ciegos ven, los mudos cantarán<br>En ti la vida está, por ti puedo cantar<br>La oscuridad se va, por ti puedo gritar<br>Libre soy</p><hr><p>Libre para correr<br>Libre para danzar<br>Libre para vivir en ti<br>Libre soy</p>";
+}
+
+
+// P letter
+function poderosoparasalvar() {
+    document.getElementById("song").innerHTML="<p><b>Poderoso para salvar</b><br>Hillsong - bpm: 73</p><hr><p>Todos necesitan amor que nunca falla<br>Tu gracia y compasión<br>Todos necesitan perdón y esperanza<br>Y un Dios que salva</p><hr><p>Cristo puede mover montes<br>Sólo Dios puede salvar, mi Dios puede salvar<br>Por siempre, Autor de Salvación<br>Jesús la muerte venció, el la muerte venció</p><hr><p>Aun con mis temores sé que me aceptas<br>Lléname otra vez<br>Mi vida te ofrezco para seguir tus pasos<br>A Tí me rindo</p><hr><p>En la tierra, Tu luz brillará<br>Cantamos por la gloria de Tu majestad, Jesús</p>";
+}
 
 
 
 // T letter
+function tempranoyotebuscare() {
+    document.getElementById("song").innerHTML="<p><b>Temprano yo te buscaré</b><br>Marcos Witt - bpm: 118</p><hr><p>Temprano yo te buscaré<br>de madrugada yo me acercaré a Ti<br>mi alma te anhela y tiene sed<br>para ver tu gloria, y tu Poder</p><hr><p>Mi socorro has sido Tú<br>en la sombra de tus alas, yo me gozaré<br>mi alma está apegada a Ti<br>por que tu diestra, me ha sostenido<br>oh, tu diestra me ha sostenido</p>";
+}
+
+
     function tuguiasmidestino() {
        document.getElementById("song").innerHTML="<p><b>Tu guías mi destino</b><br>Josue del Cid - bpm:76</p><hr><p><sub>Intro: C-Am-G-F</sub></p><p><u><small>C</small></u>Tu guías mi dest<u><small>G</small></u>in<u><small>Am</small></u>o<br>Tu guías mis p<u><small>F</small></u>asos<br><u><small>C</small></u>En ti solo conf<u><small>G</small></u>ío<u><small>Am</small></u><br>Hoy vengo a tus br<u><small>F</small></u>azos</p><hr><p><u><small>G</small></u>No tengo a n<u><small>Am</small></u>adie mas que a t<u><small>F</small></u>i<br><u><small>G</small></u>Tu eres mi raz<u><small>Am</small></u>ón de viv<u><small>F</small></u>ir</p><hr><p>La fu<u><small>C</small></u>ente de mi vida eres tu<br>Mi am<u><small>Am</small></u>igo, mi consuelo eres tu<br><u><small>G</small></u>No tengo a nadie mas que a t<u><small>Dm7</small></u>i<br><u><small>F</small></u>Tu eres mi razón de viv<u><small>G</small></u>ir<br>Solo t<u><small>Intro</small></u>u</p><p><sub>Puente: F-C-G</sub></p>";
    }
